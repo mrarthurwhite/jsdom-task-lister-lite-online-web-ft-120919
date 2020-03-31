c0e0004c80ad65b992642070f643d0e10f695466
mrarthurwhite <mister.arthur.white@gmail.com>
2020-03-31 19:22:25 -0400

    made changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", init);
+
+function init(){
+  // target the form
+  const newTaskForm = document.getElementById("create-task-form")
+  // add a listener for the form to submit!
+  newTaskForm.addEventListener("submit", addTaskToDOM)
+}
+
+function addTaskToDOM(event) {
+  event.preventDefault()
+  const taskInput = document.getElementById("new-task-description")
+  const task = taskInput.value
+  const tasksList = document.getElementById("tasks")
+    tasksList.innerHTML += `<li>${task}</li>`
+  taskInput.value =""
+}
